4be200d72ca393fdd05c8a87d50b09c5271c5f0e
Alexander <alex-koshara@mail.ru>
2016-06-21 23:21:24 +0300

    mobile end 85%

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,24 @@
 (function() {
+   var doc = document,
+       auth = doc.querySelector("#auth"),
+       flipper = doc.querySelector("#flipper"),
+       flipperHelp = doc.querySelector("#flipperHelp"),
+       returnBack = doc.querySelector("#returnBack");
 
+      console.log(auth,flipper, returnBack);
+
+       auth.addEventListener("click", function () {
+         flipper.classList.add("rotate");
+         setTimeout(function () {
+           flipperHelp.style.display = "none";
+         }, 200);
+       });
+
+       returnBack.addEventListener("click", function(e) {
+        e.preventDefault();
+        flipper.classList.remove("rotate");
+        setTimeout(function () {
+           flipperHelp.style.display = "flex";
+         }, 200);
+       });
 })();
